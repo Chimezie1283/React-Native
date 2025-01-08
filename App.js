08ae35047f8e73e1456ea7de2a3286cc7887467d
chimezie1283 <agbo.chimezie1283@gmail.com>
2025-01-08 16:08:57 +0100

    Modal

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { View, Text, Image, ScrollView, ImageBackground, Button, Pressable, Modal } from "react-native";
+
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [isModalVisible, setModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <View  style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Button 
+      title="Click Me" 
+      onPress={() => setModalVisible(true)} 
+      color="midnightblue"
+      /> 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Modal visible={isModalVisible} animationType="slide" onRequestClose={() => setModalVisible(false)} >
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
+          <Text style={{ fontSize: 30, color: "midnightblue" }}>Modal Content</Text>
+          <Button 
+          title="Close Modal"
+          onPress={() => setModalVisible(false)}
+          color="midnightblue"
+          />
+        </View>
+      </Modal>
+     {/*  <ScrollView>
+      <Pressable onPress={() => alert("Image1 Clicked")}>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </Pressable>
+      <Pressable onPress={() => alert("Text1 Clicked")}>
+              <Text>
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </Text>
+      </Pressable>
+
+<Pressable onPress={() => alert("Image2 Clicked")}>
+  <Image source={logoImg} style={{ width: 300, height: 300 }} />
+</Pressable>
+  {/*<ImageBackground source={logoImg} style={{ flex: 1 }}>
+    <Text style={{ fontSize: 30, color: "white" }}>Image Text</Text>
+  </ImageBackground>
+      </ScrollView> */}
+    
+  </View>
+
+  ) 
+
+}
