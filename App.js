e8255807ea1a0b572095499a293d3aa78a80c85e
chimezie1283 <agbo.chimezie1283@gmail.com>
2025-01-09 19:26:15 +0100

    styles-shadow

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function App() {
   return (
     <View style={ styles.container }>
-      <View style={[ styles.lightblueBg , styles.Box ]}>
+      <View style={[ styles.lightblueBg , styles.Box, styles.boxShadow, styles.andriodShadow ]}>
         <Text>Lightblue Box</Text>
       </View>
-      <View style={[ styles.lightgreenBg, styles.Box ]}>
+      <View style={[ styles.lightgreenBg, styles.Box, styles.boxShadow, styles.andriodShadow ]}>
         <Text>Lightgreen Box</Text>
       </View>
     </View>
@@ -18,15 +18,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "plum",
     padding: 60,
-    flexDirection: "row",
   },
   title: {
     fontSize: 30,
     color: "darkslateblue",
   },
   Box: {
-    width: 100,
-    height: 100,
+    width: 250,
+    height: 250,
     padding: 10,
     margin: 10,
     borderRadius: 10,
@@ -40,5 +39,17 @@ const styles = StyleSheet.create({
   },
   lightgreenBg: {
     backgroundColor: "lightgreen",
+  },
+  boxShadow: {
+    shadowColor: "black",
+    shadowOffset: {
+      width: 6, 
+      height: 6 
+    },
+    shadowOpacity: 0.6,
+    shadowRadius: 4,
+  },
+  andriodShadow: {
+    elevation: 10,
   },
 });
