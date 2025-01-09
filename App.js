74e57eb8acccb9eb86e3af66990e6d7a1a195741
chimezie1283 <agbo.chimezie1283@gmail.com>
2025-01-09 07:47:33 +0100

    Box Model

--- App.js
+++ App.js
@@ -3,7 +3,12 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function App() {
   return (
     <View style={ styles.container }>
-      <Text style={ styles.title}>styleSheet API</Text>
+      <View style={[ styles.lightblueBg , styles.Box ]}>
+        <Text>Lightblue Box</Text>
+      </View>
+      <View style={[ styles.lightgreenBg, styles.Box ]}>
+        <Text>Lightgreen Box</Text>
+      </View>
     </View>
   );
 }
@@ -13,9 +18,27 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "plum",
     padding: 60,
+    flexDirection: "row",
   },
   title: {
     fontSize: 30,
     color: "darkslateblue",
+  },
+  Box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+    borderColor: "darkslateblue",
+    borderWidth: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightgreenBg: {
+    backgroundColor: "lightgreen",
   },
 });
